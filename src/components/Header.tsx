7bcdf45e6bfb01bdeef4c04a5a02939bbf43863e
Italo Araujo <italojsouza@gmail.com>
2023-05-29 20:40:10 -0300

    Iniciando o portfolio

---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable max-len */
+export default function Header() {
+  return (
+    <div>
+
+      <h1>  Ítalo Jean Araújo de Souza  </h1>
+
+      <img src="src/assets/ITALOPERFIL.png" alt="Foto Ítalo Sorrindo" />
+
+      <h2 id="descricao"> Descrição </h2>
+
+      <p className="conteudo">
+        {' '}
+        <em>
+          {' '}
+          Sou o Ítalo Araújo, um desenvolvedor
+          em treinamento pela Trybe e desde pequeno bastante curioso e com paixão de aprender, sempre
+          tive afinidade com a lógica computacional e desenvolvimento, nascido e residente de Belo Horizonte, Minas Gerais, Brasil.
+          {' '}
+        </em>
+        {' '}
+
+      </p>
+
+    </div>
+  );
+}
