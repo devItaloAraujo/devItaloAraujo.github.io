054219a84074eefe57144b4d6299bbb1fcadad83
Italo Araujo <italojsouza@gmail.com>
2023-06-12 12:43:29 -0300

    Adicionei projeto pixels art

--- src/assets/cards.ts
+++ src/assets/cards.ts
@@ -1,16 +1,16 @@
 /* eslint-disable max-len */
 const cards = [
-  { id: '1',
-    title: 'Card Heading',
-    img: 'https://images.unsplash.com/photo-1656618020911-1c7a937175fd?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTc1MzQyNTE&ixlib=rb-1.2.1&q=80',
-    link: 'https://www.google.com/',
-    text: 'Aqui vai o texto, tipo aquele Lorem IPSUM Lorem , Ipsum , Ipsum Lorem',
-  },
   { id: '2',
     title: 'Gerenciador de Senhas',
     img: 'src/assets/images/password-manager.png',
     link: 'https://www.google.com/',
     text: 'Password-manager: Aplicativo capaz de cadastrar e salvar dados de login de diversos serviços',
+  },
+  { id: '3',
+    title: 'Pixels Art',
+    img: 'src/assets/images/pixels-art.png',
+    link: 'https://devitaloaraujo.github.io/Paleta-de-cores-pixels-art/',
+    text: 'Arte com pixels: Pinte com pixels de diversas cores, mude o tamanho da paleta, gere novas cores aleatoriamente',
   },
 ];
 
